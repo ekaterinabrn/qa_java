3ecb298a095afa1b2e3598ef836b0af1a5d4c6e3
Ekaterina Briuchanova <ueobrn@yandex.ru>
2024-09-09 16:35:31 +0700

    add lion test param

--- src/test/java/practicum/LionTest.java
+++ src/test/java/practicum/LionTest.java
@@ -1,4 +1,21 @@
 package practicum;
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+    @Mock
+    Feline feline;
+    @Test
+    public void lionHasVoidGetKittens(){
+
+    }
+
 }
---
+++ src/test/java/practicum/LionTestWithParam.java
@@ -0,0 +1,38 @@
+package practicum;
+import com.example.Feline;
+import com.example.Lion;
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionTestWithParam {
+    private String sex;
+    private boolean hasMane;
+public LionTestWithParam(String sex, boolean hasMane){
+    this.sex=sex;
+    this.hasMane=hasMane;
+}
+    @Mock
+    Feline feline;
+    @Parameterized.Parameters
+    public static Object[][] setSexForLion() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+    @Test
+    public void doesHaveManeTest() throws Exception {
+        Feline feline = new Feline();
+        Lion lion = new Lion(feline, sex);
+        assertEquals("Грива у самца",hasMane, lion.doesHaveMane());
+    }
+
+}
